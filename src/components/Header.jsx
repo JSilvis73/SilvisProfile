64e27a888a2d47d962594fbb19960ab74492003e
JSilvis <SilvisDevelopment@gmail.com>
2024-07-12 08:16:13 -0400

    Modified the HomePage to display the header. Modified the Header and added a basic style to test TW.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <div>
-      Header
-    </div>
+    <header className='bg-blue-600 text-white p-4 rounded-xl'>
+        <h1 className=''>My Profile</h1>
+    </header>
   )
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Header from '../components/Header'
 
 export default function HomePage() {
   return (
     <div>
-      <h1>Home Page</h1>
+      <Header />
     </div>
   )
 }
