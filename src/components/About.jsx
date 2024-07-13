5b21eb53a9ef41bbf9c301f3593d0e11b914e345
JSilvis <SilvisDevelopment@gmail.com>
2024-07-13 12:02:28 -0400

    Added a basic picture and layout to About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import walkingPic from "../assets/images/walking.jpg"
 
 export default function About() {
   return (
-    <div>
-      About
+    <div className='bg-blue-500'>
+         <h3>Jason Silvis</h3>
+        <img src={walkingPic} alt='An image of me!'/>
+     <p>This paragraph will be used to talk about me</p>
     </div>
   )
 }
