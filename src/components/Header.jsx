7d42675836e43a015745116c6dcf8b475753926b
JSilvis <SilvisDevelopment@gmail.com>
2024-07-13 11:40:18 -0400

    Creating a better branch header more appropriate to the name

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,7 @@ export default function Header() {
   return (
     <header className='bg-blue-600 text-white p-4 rounded-xl'>
         <h1 className=''>Silvis Software</h1>
+        <p>Software with care!</p>
     </header>
   )
 }
