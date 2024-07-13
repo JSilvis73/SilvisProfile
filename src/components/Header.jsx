427b39220440ae8953ef0c7a7ad6b2a068c6b7fe
JSilvis <SilvisDevelopment@gmail.com>
2024-07-13 11:37:57 -0400

    Added components to home page to see.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Header() {
   return (
     <header className='bg-blue-600 text-white p-4 rounded-xl'>
-        <h1 className=''>My Profile</h1>
+        <h1 className=''>Silvis Software</h1>
     </header>
   )
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Header from '../components/Header'
+import About from '../components/About'
+import Footer from '../components/Footer'
+import Portfolio from '../components/Portfolio'
 
 export default function HomePage() {
   return (
     <div>
       <Header />
+      <About />
+      <Portfolio />
+      <Footer />
     </div>
   )
 }
