764b2bbb4cf178485e2cd90e02848b3041b5c3de
JSilvis <SilvisDevelopment@gmail.com>
2024-07-17 07:45:46 -0400

    Styling the Portfolio element: bg, border-radius, text.

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -4,9 +4,9 @@ import ProjectCard from './ProjectCard';
 
 export default function Portfolio() {
   return (
-    <div>
+    <div className='bg-slate-600 rounded-lg mt-2 p-4 w-full text-white'>
       Portfolio
-      <div>
+      <div >
         <ProjectCard title='Jasons Project' imgSrc='' imgAlt='Jasons project here' description='Description of Project' />        
         <ProjectCard title='Jasons Project #2' imgSrc='' imgAlt='Jasons second project here' description='Description of Second Project' />        
       </div>
