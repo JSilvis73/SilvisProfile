cd3acbabc4c1d2c4c314f4eefe25d5c987a90bff
JSilvis <SilvisDevelopment@gmail.com>
2024-07-18 07:58:33 -0400

    Added styling to the About component. Still a WIP.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,10 +3,12 @@ import walkingPic from "../assets/images/walking.jpg"
 
 export default function About() {
   return (
-    <div className='bg-blue-500'>
-         <h3>Jason Silvis</h3>
+    <div className='bg-slate-600 rounded-lg text-white m-4 p-4'>
+      <div className='container mx-auto'>
+         <h3 className='text-3xl font-bold'>Jason Silvis</h3>
         <img src={walkingPic} alt='An image of me!'/>
      <p>This paragraph will be used to talk about me</p>
+     </div>
     </div>
   )
 }
