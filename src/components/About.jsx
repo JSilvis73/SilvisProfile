e2b70f09c9f9977a7d9466b5452a5ef140746f8e
JSilvis <SilvisDevelopment@gmail.com>
2024-07-18 08:24:07 -0400

    Fixed header component margin from top. Styled About more

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ import walkingPic from "../assets/images/walking.jpg"
 export default function About() {
   return (
     <div className='bg-slate-600 rounded-lg text-white m-4 p-4'>
-      <div className='container mx-auto'>
+      <div className='container mx-auto p-4'>
          <h3 className='text-3xl font-bold'>Jason Silvis</h3>
         <img className='size-60' src={walkingPic} alt='An image of me!'/>
      <p>This paragraph will be used to talk about me</p>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,8 @@ import Navbar from './UI/Navbar.jsx';
 
 const Header = () => {
   return (
-    <header className="relative bg-blue-500 text-white shadow-lg">
-      <div className="container mx-auto flex justify-between items-center p-4">
+    <header className="relative bg-slate-600 rounded-lg text-white shadow-lg mt-8">
+      <div className="container mx-auto flex justify-between items-center  p-4">
         <div className="flex items-center">
           <img src="/assets/images/logo.png" alt="Logo" className="h-10 w-10" />
           <h1 className="text-3xl ml-2">My Profile</h1>
