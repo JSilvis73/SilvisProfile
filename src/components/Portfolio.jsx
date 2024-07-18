59b7e924b9742f69c8ef1e70f14ee1300be76f08
JSilvis <SilvisDevelopment@gmail.com>
2024-07-17 22:37:19 -0400

    Added styling and refactored the project cards.

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import ProjectCard from './ProjectCard';
 
-
-export default function Portfolio() {
+const Portfolio = () => {
   return (
-    <div className='bg-slate-600 rounded-lg mt-2 p-4 w-full text-white'>
-      Portfolio
-      <div >
-        <ProjectCard title='Jasons Project' imgSrc='' imgAlt='Jasons project here' description='Description of Project' />        
-        <ProjectCard title='Jasons Project #2' imgSrc='' imgAlt='Jasons second project here' description='Description of Second Project' />        
+    <div className="bg-slate-600 rounded-lg mt-2 p-4 text-white">
+      <div className="container mx-auto">
+        <h2 className="text-3xl font-bold mb-4">Portfolio</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          <ProjectCard 
+            title="Jason's Project" 
+            imgSrc="path/to/your/image.jpg" 
+            imgAlt="Jason's project here" 
+            description="Description of Project" 
+          />
+          <ProjectCard 
+            title="Jason's Project #2" 
+            imgSrc="path/to/your/image2.jpg" 
+            imgAlt="Jason's second project here" 
+            description="Description of Second Project" 
+          />
+         
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
+
+export default Portfolio;
