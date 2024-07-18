cf2fbd36cc37c44f9afad1428d6e86f97bde8fff
JSilvis <SilvisDevelopment@gmail.com>
2024-07-18 08:07:23 -0400

    Sizing the image in the About component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ export default function About() {
     <div className='bg-slate-600 rounded-lg text-white m-4 p-4'>
       <div className='container mx-auto'>
          <h3 className='text-3xl font-bold'>Jason Silvis</h3>
-        <img src={walkingPic} alt='An image of me!'/>
+        <img className='size-60' src={walkingPic} alt='An image of me!'/>
      <p>This paragraph will be used to talk about me</p>
      </div>
     </div>
