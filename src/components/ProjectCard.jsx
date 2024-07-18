fcde43503b40f7fa3360fbd06fd27a534977fb66
JSilvis <SilvisDevelopment@gmail.com>
2024-07-17 22:59:59 -0400

    Changed position of the title. Added margin to the top of the paragraph tag

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -5,11 +5,12 @@ export default function ProjectCard({title, imgSrc, imgAlt, description}) {
   return (
     <div className='bg-slate-500 rounded-lg shadow-lg overflow-hidden m-4'>
       <div>
-      <h5>{title}</h5>
+      
       {imgSrc && <img src={imgSrc} alt={imgAlt} className='w-full h-48 object-cover'/>}
       
       <div className='p-2'>
-        <p>{description}</p>
+      <h5 className='text-xl font-bold'>{title}</h5>
+        <p className='mt-2'>{description}</p>
         </div>
 
       </div>
