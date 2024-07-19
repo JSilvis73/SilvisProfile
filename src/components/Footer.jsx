0a12114d2b34c8738de05a64591b58012c863586
JSilvis <SilvisDevelopment@gmail.com>
2024-07-19 14:25:47 -0400

    Styling the footer. Added a background and centered text.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 export default function Footer() {
   return (
-    <div className='fixed bottom-0 w-screen'>
-      Footer
-    </div>
-  )
+    <footer className="bg-slate-600 text-white p-2 rounded-lg">
+      <div className="">
+        <p>Footer</p>
+      </div>
+    </footer>
+  );
 }
