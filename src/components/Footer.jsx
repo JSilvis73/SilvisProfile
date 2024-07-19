eabd93080661edaa209a09ec4fd9257b5adc7112
JSilvis <SilvisDevelopment@gmail.com>
2024-07-19 14:28:26 -0400

    Added a copyright style  to make it look more authentic.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,8 @@ import React from "react";
 export default function Footer() {
   return (
     <footer className="bg-slate-600 text-white p-2 rounded-lg">
-      <div className="">
-        <p>Footer</p>
+      <div className="container mx-auto text-center">
+        <p>&copy; 2024 Silvis Development</p>
       </div>
     </footer>
   );
