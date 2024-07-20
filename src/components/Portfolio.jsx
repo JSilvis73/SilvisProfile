cc47fd045c3344786ac9b35037b42fb7f3edbba9
JSilvis <SilvisDevelopment@gmail.com>
2024-07-20 02:07:26 -0400

    Reduced gap between flex items.

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -7,7 +7,7 @@ const Portfolio = () => {
     <div className="bg-slate-600 rounded-lg m-2 p-4 text-white shadow-lg">
       <div className="container mx-auto">
         <h2 className="text-3xl font-bold mb-4">Portfolio</h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
           <ProjectCard 
             title="Jason's Project" 
             imgSrc={walkingPic}
