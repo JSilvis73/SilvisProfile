787e39a08740e5beb1f3ecc15e1b530ca266c363
JSilvis <SilvisDevelopment@gmail.com>
2024-07-20 13:09:38 -0400

    Lot of styling changes. Had some ideas for a glow effect instead around the About div. Trying to make it look like a futuristic computer monitor.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,28 +1,35 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import walkingPic from "../assets/images/walking.jpg";
 
 export default function About() {
-  const [isFlickering, setIsFlickering] = useState(false);
+  // const [isFlickering, setIsFlickering] = useState(false);
 
-  useEffect(() => {
-    const flickerInterval = setInterval(() => {
-      // Trigger flicker
-      setIsFlickering(true);
-      setTimeout(() => setIsFlickering(false), 50); // Flicker duration (0.05 seconds)
+  // useEffect(() => {
+  //   const normalDuration = 100000; // 10 seconds of normal state
+  //   const flickerDuration = 1000; // Flicker lasts for 1 second
 
-      // Reset flicker every 10 to 15 seconds
-    }, Math.random() * 5000 + 10000); // Random interval between 10-15 seconds
+  //   const flickerInterval = setInterval(() => {
+  //     setIsFlickering(true);
 
-    return () => clearInterval(flickerInterval);
-  }, []);
+  //     // Stop flickering after a short duration
+  //     setTimeout(() => setIsFlickering(false), flickerDuration);
+
+  //     // Continue to flicker at regular intervals
+  //   }, normalDuration + flickerDuration); // Start flickering after normal duration
+
+  //   return () => clearInterval(flickerInterval);
+  // }, []);
 
   return (
-    <div className={`about-container ${isFlickering ? 'flicker' : ''}`}>
-      <div className='container mx-auto p-4'>
-        <h3 className='text-3xl font-bold'>Jason Silvis</h3>
-        <img className='size-60' src={walkingPic} alt='An image of me!'/>
-        <p>This paragraph will be used to talk about me</p>
+    <div className="">
+      <div className="container mx-auto m-2 p-4 bg-scifi/50 rounded-lg bg-glow text-white">
+      <h3 className="text-3xl font-bold">Jason Silvis</h3>
+        <div className="flex flex-col md:flex-row">
+          <img className="size-60 " src={walkingPic} alt="An image of me!" />
+          <p>This paragraph will be used to talk about me</p>
+        </div>
       </div>
     </div>
   );
+  //className={`about-container ${isFlickering ? 'flicker' : 'normal'}`}
 }
