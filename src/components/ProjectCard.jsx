399fb1353abf5958e209f23e238d2df08e0d434f
JSilvis <SilvisDevelopment@gmail.com>
2024-07-20 02:08:21 -0400

    Format of ProjectCard.jsx

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,19 +1,18 @@
-import React from 'react';
+import React from "react";
 
-
-export default function ProjectCard({title, imgSrc, imgAlt, description}) {
+export default function ProjectCard({ title, imgSrc, imgAlt, description }) {
   return (
-    <div className='bg-slate-500 rounded-lg shadow-lg overflow-hidden m-4'>
+    <div className="bg-slate-500 rounded-lg shadow-lg overflow-hidden m-4">
       <div>
-      
-      {imgSrc && <img src={imgSrc} alt={imgAlt} className='w-full h-48 object-cover'/>}
-      
-      <div className='p-2'>
-      <h5 className='text-xl font-bold'>{title}</h5>
-        <p className='mt-2'>{description}</p>
-        </div>
+        {imgSrc && (
+          <img src={imgSrc} alt={imgAlt} className="w-full h-48 object-cover" />
+        )}
 
+        <div className="p-2">
+          <h5 className="text-xl font-bold">{title}</h5>
+          <p className="mt-2">{description}</p>
+        </div>
       </div>
     </div>
-  )
+  );
 }
