06ef740f0965bf9e4a2b40313d75544370fa7018
JSilvis <SilvisDevelopment@gmail.com>
2024-07-21 00:00:03 -0400

    Adjusted size of navbar

--- src/components/UI/Navbar.jsx
+++ src/components/UI/Navbar.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import '../styles/Navbar.css'
 
 const Navbar = () => {
   return (
-    <nav className="navbar-light fixed top-0 left-0 w-full p-4 bg-neutral-300 rounded-lg bg-glow text-white">
-    <div className="container mx-auto flex justify-end ">
+    <nav className=" navbar-light fixed top-2 left-1/4  w-1/2 p-4 bg-neutral-300/75 rounded-lg bg-glow">
+    <div className="container mx-auto flex justify-center ">
       <a href="#about" className="mx-2">About</a>
       <a href="#portfolio" className="mx-2">Portfolio</a>
       <a href="#contact" className="mx-2">Contact</a>
