5e93ef9ab86a0db0802f41d1e62d9f4b4e1154ef
JSilvis <SilvisDevelopment@gmail.com>
2024-08-18 11:56:15 -0400

    Removed some of the clutter to clarify some content. Mainly the background color of the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,10 @@ import Navbar from './UI/Navbar.jsx';
 
 const Header = () => {
   return (
-    <header className="w-full  p-4 bg-scifi/50 rounded-lg bg-glow text-white">
+    <header className="w-full  p-4 text-white">
       <div className="container mx-auto flex justify-center  p-4">
         <div className="flex items-center">
-          <img src="/assets/images/logo.png" alt="Logo" className="h-10 w-10" />
+          
           <h2 className="text-3xl font-bold ml-2 ">Imagination to Creation</h2>
         </div>
       </div>
