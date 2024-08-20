b9ec7fd68a46adcb1e229bcc922926ae15cc7b78
JSilvis <SilvisDevelopment@gmail.com>
2024-08-20 08:36:10 -0400

    Added react router for better navigation and I plan on showing the components on the home page but a better page for each component later

--- src/components/UI/Navbar.jsx
+++ src/components/UI/Navbar.jsx
@@ -2,24 +2,29 @@ import React from "react";
 import "../styles/Navbar.css";
 import {faFacebook, faDiscord } from "@fortawesome/free-brands-svg-icons"
 import { FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <nav className=" backdrop-blur-sm  navbar-light fixed top-0 p-4 w-full left-0 z-50 md:bg-slate-500/95  md:w-1/2 md:left-1/4 rounded-b-lg ">
       <div className="container mx-auto flex flex-row ">
         <div className="">
-        <a href="#about" className="mx-2">
+        <Link to="/"  className="mx-2">
+          Home
+        </Link>
+        <Link to="/about" className="mx-2">
           About
-        </a>
-        <a href="#skills" className="mx-2">
+        </Link>
+        <Link to="/skills"  className="mx-2">
           Skills
-        </a>
-        <a href="#portfolio" className="mx-2">
+        </Link>
+        <Link to="/portfolio"  className="mx-2">
           Portfolio
-        </a>
-        <a href="#contact" className="mx-2">
+        </Link>
+        <Link to="/contact"  className="mx-2">
           Contact
-        </a>
+        </Link>
+
         </div>
         <div className="ml-auto  flex border-2 border-scifi/50 rounded-xl">
           <a><FontAwesomeIcon className="mx-2" icon={faFacebook} /></a>
