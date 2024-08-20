98651aabb869e7e8e46a911cb336e8f0ed059caa
JSilvis <SilvisDevelopment@gmail.com>
2024-08-20 08:38:45 -0400

    Modified contact page a little. added navbar

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import Navbar from '../components/UI/Navbar'
 
 export default function ContactPage() {
   return (
     <div>
+        <nav className="nav-wrapper ">
+        <Navbar />
+      </nav>
+      <div className='mt-16'>
       Contact
+      </div>
     </div>
   )
 }
