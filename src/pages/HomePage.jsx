2ba242586730e58d5aff9f754dd5aa486d5965c2
JSilvis <SilvisDevelopment@gmail.com>
2024-08-27 17:51:28 -0400

    Making a video to play in the back

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,15 +5,29 @@ import Footer from "../components/Footer";
 import Portfolio from "../components/Portfolio";
 import Navbar from "../components/UI/Navbar";
 import Skills from "../components/Skills";
+import codeVid from '../assets/videos/codingvid.mp4'
+import "./styles/HomePage.css";
 
 export default function HomePage() {
   return (
     <div className="container mx-auto w-full p-4 ">
-      <nav className="nav-wrapper ">
+         
+            <nav className="nav-wrapper ">
         <Navbar />
       </nav>
+
+
       <main className="mt-16 ">
         <div className="flex flex-col text-white min-h-screen w-screen justify-center align-center">
+          
+      <video 
+      className="top-0"
+      src={codeVid}
+      autoPlay
+      loop
+      muted
+      />
+          
           <p>
           Home
           </p>
